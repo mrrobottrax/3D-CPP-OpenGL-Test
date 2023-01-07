89963426bdf061e14781fb9bb6d8d85fa7525d08
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2023-01-07 18:47:23 -0500

    Add premake

---
+++ premake5.lua
@@ -0,0 +1,78 @@
+workspace "3D"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Test",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "3D"
+	location "3D"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir    ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/include",
+		"%{prj.name}/src"
+	}
+
+	libdirs
+	{
+		"%{prj.name}/lib"
+	}
+	links
+	{
+		"opengl32.lib",
+		"glew32s.lib",
+		"glfw3.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+
+		}
+
+		postbuildcommands
+		{
+
+		}
+
+	filter "configurations:Debug"
+		defines ""
+		symbols "On"
+
+	filter "configurations:Test"
+		defines ""
+		optimize "On"
+
+	filter "configurations:Release"
+		defines ""
+		optimize "On"
+
+	filter { "system:windows", "configurations:Debug" }
+		buildoptions "/MDd"
+
+	filter { "system:windows", "configurations:Test" }
+		buildoptions "/MD"
+
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MD"
