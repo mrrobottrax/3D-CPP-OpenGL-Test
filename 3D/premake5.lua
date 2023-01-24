555c79c49595f9fe09a20b233f48e5680bcc8c62
AH <mrrobottrax@gmail.com>
2023-01-24 09:43:34 -0500

    Add premake file for main project

---
+++ 3D/premake5.lua
@@ -0,0 +1,71 @@
+project "3D"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir    ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "%{wks.location}/3D/src/engine/pch.cpp"
+
+	files
+	{
+		"%{wks.location}/3D/src/**.h",
+		"%{wks.location}/3D/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/3D/vendor/src",
+		"%{wks.location}/3D/src/engine",
+		"%{wks.location}/3D/src",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.IMGUI}",
+		"%{IncludeDir.JSON}",
+		"%{IncludeDir.GLM}",
+	}
+
+	libdirs
+	{
+		
+	}
+
+	links
+	{
+		"opengl32.lib",
+		"Glad",
+		"GLFW",
+		"IMGUI",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"_GLFW_WIN32"
+		}
+
+	filter "configurations:Debug"
+		defines ""
+		symbols "On"
+
+	filter "configurations:Test"
+		defines ""
+		optimize "On"
+
+	filter "configurations:Release"
+		defines ""
+		optimize "On"
+
+	filter { "system:windows", "configurations:Debug" }
+		runtime "Debug"
+
+	filter { "system:windows", "configurations:Test" }
+		runtime "Release"
+
+	filter { "system:windows", "configurations:Release" }
+		runtime "Release"
--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "3D"
 	architecture "x64"
+	startproject "3D"
 
 	configurations
 	{
@@ -11,85 +12,13 @@ workspace "3D"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
-IncludeDir["Glad"] = "3D/vendor/Glad/include"
-IncludeDir["GLFW"] = "3D/vendor/GLFW/include"
-IncludeDir["IMGUI"] = "3D/vendor/IMGUI"
-IncludeDir["JSON"] = "3D/vendor/JSON"
-IncludeDir["GLM"] = "3D/vendor/GLM"
+IncludeDir["Glad"]  = "%{wks.location}/3D/vendor/Glad/include"
+IncludeDir["GLFW"]  = "%{wks.location}/3D/vendor/GLFW/include"
+IncludeDir["IMGUI"] = "%{wks.location}/3D/vendor/IMGUI"
+IncludeDir["JSON"]  = "%{wks.location}/3D/vendor/JSON"
+IncludeDir["GLM"]   = "%{wks.location}/3D/vendor/GLM"
 
+include "3D"
 include "3D/vendor/Glad"
 include "3D/vendor/GLFW"
 include "3D/vendor/IMGUI"
-
-project "3D"
-	location "3D"
-	kind "ConsoleApp"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir    ("obj/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "pch.h"
-	pchsource "%{prj.name}/src/engine/pch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/vendor/src",
-		"%{prj.name}/src/engine",
-		"%{prj.name}/src",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.IMGUI}",
-		"%{IncludeDir.JSON}",
-		"%{IncludeDir.GLM}",
-	}
-
-	libdirs
-	{
-		
-	}
-
-	links
-	{
-		"opengl32.lib",
-		"Glad",
-		"GLFW",
-		"IMGUI",
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"_GLFW_WIN32"
-		}
-
-	filter "configurations:Debug"
-		defines ""
-		symbols "On"
-
-	filter "configurations:Test"
-		defines ""
-		optimize "On"
-
-	filter "configurations:Release"
-		defines ""
-		optimize "On"
-
-	filter { "system:windows", "configurations:Debug" }
-		runtime "Debug"
-
-	filter { "system:windows", "configurations:Test" }
-		runtime "Release"
-
-	filter { "system:windows", "configurations:Release" }
-		runtime "Release"
